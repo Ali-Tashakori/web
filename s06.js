4627e4578805afef9b416c61925a5999b1f15552
Ali-Tashakori <147505037+Ali-Tashakori@users.noreply.github.com>
2023-11-07 06:02:40 -0800

    requesr&esponse

--- s06.js
+++ s06.js
@@ -1,17 +1,40 @@
 let http= require ('http');
+let port = (80);
 let server = http.createServer(requestHandler);
-server.listen(80);
-let headers ={'Content-Type':'Text/plane'}
+server.listen(port);
+console.log("server is running on port"+port);
+let headers ={'Content-Type':'Text/plane'};
+
+let obj={
+x:function(response){
+console.log('montazeri')
+response.writeHead(200,headers);
+response.write("name daneshgah:");
+
+},
+y:function(response){
+    console.log('mamad')
+    response.writeHead(200,headers);
+    response.write("name daneshgah:");
+
+
+},
+"favicon.ico": function(){
+console.log('favicon')
+
+}
+}
 
 function requestHandler(request,response){
-console.log('request url:',request.url);
-console.log('request method:',request.method);
+//console.log('request url:',request.url);
+//console.log('request method:',request.method);
+let firstpart= request.url.split('/')[1];
 
+obj [firstpart](response);
 
 response.writeHead(200,headers);
-response.write('salam'+request.url);
+response.write("salam");
 response.end();
-
 
 
 }
