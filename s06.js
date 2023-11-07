95b413b3e320f67dd00b7e2d78b5515951d52c81
Ali-Tashakori <147505037+Ali-Tashakori@users.noreply.github.com>
2023-11-07 04:40:09 -0800

    createserver

---
+++ s06.js
@@ -0,0 +1,17 @@
+let http= require ('http');
+let server = http.createServer(requestHandler);
+server.listen(80);
+let headers ={'Content-Type':'Text/plane'}
+
+function requestHandler(request,response){
+console.log('request url:',request.url);
+console.log('request method:',request.method);
+
+
+response.writeHead(200,headers);
+response.write('salam'+request.url);
+response.end();
+
+
+
+}
